ca80b1d9e2bbb2e5c3811b86d777c4abbbf00df6
BaronZ <realbo89@outlook.com>
2016-08-04 17:40:51 +0800

    init commit

---
+++ app/src/main/java/com/zzb/drawingtest/canvas/CanvasActivity.java
@@ -0,0 +1,15 @@
+package com.zzb.drawingtest.canvas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zzb.drawingtest.R;
+
+public class CanvasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canvas);
+    }
+}
---
+++ app/src/main/java/com/zzb/drawingtest/surfaceview/SurfaceViewActivity.java
@@ -0,0 +1,15 @@
+package com.zzb.drawingtest.surfaceview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zzb.drawingtest.R;
+
+public class SurfaceViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surface_view);
+    }
+}
